701b5a717655c3c5436015a5adbdf51a76600b8c
edulach <158227946+edulach@users.noreply.github.com>
2025-12-17 17:22:37 -0300

    Update Application.R

--- Application.R
+++ Application.R
@@ -13,7 +13,7 @@ library(Matrix)
 library(readxl)
 
 # 2. Load dataset (automatic file detection if only 1 xlsx exists)
-data<-"Data_agriculture_v2.xlsx"
+data<-"Dados_Completo_Pib_e_Covarivais.xlsx"
 data_path <- file.path(base_dir, "dataset", data)
 
 if(length(data_path) > 1) {
@@ -28,17 +28,41 @@ message("\nData loaded: ", basename(data_path))
 print(summary(data))
 
 #3. Matrix X and Y
-Y<-t(rbind(data[, 3:5]))
-X_matrix <- t(rbind(data[, 6:8]))
+Y<-t(rbind(data[, 3:6]))
+X <- t(rbind(data[, 7:9]))
 
+#4. transformations
+Y <- log(Y)
+X[1, ] <- log(X[1, ])# transformation log for population variable
 #4. coordinates
 coords <- matrix(c(
-  0.000,     0.000,
-  -51.747,    5.547,
-  -415.144,   25.870,
-  -662.125,  -55.056,
-  -582.202, -105.580,
-  -612.205,  -12.316
+  0.00,     0.00,
+  -141.25, -100.08,
+  -874.23,   20.02,
+  -719.07, -518.17,
+  -2127.25, 646.04,
+  -335.99, 1759.10,
+  -1293.68, 1409.95,
+  -60.99, 1592.31,
+  -1708.86, 780.59,
+  -1363.24, 2068.23,
+  -46.01, 619.36,
+  1305.45, 679.40,
+  1007.98, 312.46,
+  1004.77, 1341.01,
+  388.43, 1473.33,
+  1398.55, 962.95,
+  1396.41, 859.54,
+  548.93, 1188.67,
+  1361.09, 1109.73,
+  1162.07, 541.52,
+  812.16, -504.82,
+  426.95, -460.35,
+  505.06, -791.71,
+  138.04, -863.98,
+  -143.39, -1073.03,
+  -353.11, -1584.53,
+  -66.34, -1314.32
 ), ncol = 2, byrow = TRUE)
 
 colnames(coords) <- c("x", "y")
@@ -47,5 +71,5 @@ coords
 #5. Run Spatio-Temporal regression
 source(file.path(base_dir, "codes", "Functions SpatioFile.R"))
 
-ResC<-ML.MatrixRegreSPatioT(Y, X_matrix, coords, precision=0.000001, MaxIter=100, corr="exponential")
+ResC<-ML.MatrixRegreSPatioT(Y, X, coords, precision=1e-12, MaxIter=100, corr="exponential")
 ResC
