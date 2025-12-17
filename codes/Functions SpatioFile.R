5f4bf4d5582e63550e7ff0885df9be4f29e4033a
edulach <158227946+edulach@users.noreply.github.com>
2025-12-17 17:23:04 -0300

    Update Functions SpatioFile.R

--- codes/Functions SpatioFile.R
+++ codes/Functions SpatioFile.R
@@ -18,6 +18,9 @@ spatial_corr_matrix <- function(coords, corr_fun = "exponential", phi, nu = NULL
   coords <- as.matrix(coords)
   # Compute the Euclidean distance matrix.
   D <- as.matrix(dist(coords))
+  #xtable(as.matrix(dist(coords)))
+  #xtable(as.matrix(dist(coords)), align = "r", digits = 3)
+  #D <- D/ max(D, na.rm = TRUE)
   
   if (tolower(corr_fun) == "exponential") {
     # Exponential: rho(d) = exp(-d/phi)
@@ -37,23 +40,19 @@ spatial_corr_matrix <- function(coords, corr_fun = "exponential", phi, nu = NULL
     Corr[r >= 1] <- 0
     
   } else if (tolower(corr_fun) == "matern") {
-    # Mat?rn: requires a smoothness parameter nu.
     if (is.null(nu)) {
       stop("For matern correlation, please provide the nu parameter (smoothness).")
     }
-    # Mat?rn correlation:
-    #   rho(d) = (2^(1-nu)/gamma(nu)) * (d/phi)^nu * besselK(d/phi, nu),
-    # with the convention that rho(0) = 1.
     r <- D / phi
-    Corr <- matrix(0, nrow = nrow(D), ncol = ncol(D))
-    Corr <- (2^(1 - nu) / gamma(nu)) *
-      (scaled_D[nonzero]^nu) *
-      besselK(scaled_D[nonzero], nu)
-    
+    Corr <- (1 / (2^(nu - 1) * gamma(nu))) * (r^nu) * besselK(r, nu)
+    Corr[is.na(Corr) | is.infinite(Corr)] <- 1
   } else {
     stop("Unknown corr_fun. Please use 'exponential', 'gaussian', or 'matern'.")
   }
   
+  # Ensure symmetry and diagonal = 1
+  Corr <- (Corr + t(Corr)) / 2 ##symmetry
+  diag(Corr) <- 1
   return(Corr)
 }
 
@@ -65,7 +64,7 @@ loglikel<-function(dados, mu, Sigma, PsiT, PsiS){
   ## PsiT: temporal dependence
   ## PsiS: spatial dependence
   Psi<- kronecker(PsiS, PsiT)
-  like<- LaplacesDemon::dmatnorm(dados, mu, Sigma, Psi, tol = .Machine$double.eps^0.5, log = TRUE)
+  like<- matrixNormal::dmatnorm(dados, mu, Sigma, Psi, tol = .Machine$double.eps^0.5, log = TRUE)
   return(like)
 }
 
@@ -73,20 +72,29 @@ loglikel<-function(dados, mu, Sigma, PsiT, PsiS){
 loglikelSpatiorho<-function(rho, PsiS, Sigma, dados, mu, TT){
   PsiT<-ar1_cor(TT,rho)
   Psi<-kronecker(PsiS, PsiT)
-  #Psi <- (Psi + t(Psi)) / 2
-  
+  Psi <- (Psi + t(Psi)) / 2
   like<-matrixNormal::dmatnorm(dados, mu, Sigma, Psi, tol = .Machine$double.eps^0.5, log = TRUE)
   return(like)
 }
 
 
-loglikelSpatiophi<-function(phi, PsiT, Sigma, sigma2, dados, coords, mu, corr="exponential"){
-  PsiS<-spatial_corr_matrix(coords, corr_fun = corr, phi)*sigma2
+loglikelSpatiophi<-function(phi, PsiT, Sigma, sigma2, dados, coords, mu, corr="exponential",nu = NULL){
+  #phi <- exp(theta)
+  PsiS<-spatial_corr_matrix(coords, corr_fun = corr, phi, nu = nu)*sigma2
+  # Add small nugget for stability
+  #epsilon <- 1e-6
+  #PsiS <- (PsiS + t(PsiS)) / 2
+  #diag(PsiS) <- 1 + epsilon
+  # Same for temporal matrix
+  #PsiT <- (PsiT + t(PsiT)) / 2
+  #diag(PsiT) <- 1 + epsilon
   Psi<-kronecker(PsiS, PsiT)
-  #Psi <- (Psi + t(Psi)) / 2
+  Psi <- (Psi + t(Psi)) / 2
   like<-matrixNormal::dmatnorm(dados, mu, Sigma, Psi, tol = .Machine$double.eps^0.5, log = TRUE)
   return(like)
 }
+
+
 
 
 
@@ -109,10 +117,10 @@ GeraMatrixFull<-function(M=M, coords, phi, rho, sigma2, Sigma, corr="exponential
   T<-r/L
   
   PsiT <- ar1_cor (T, rho = rho)
-  PsiS<- spatial_corr_matrix(coords, corr_fun = corr, phi)*sigma2
+  PsiS<- spatial_corr_matrix(coords, corr_fun = corr, phi,nu)*sigma2
   Psi<- kronecker(PsiS, PsiT)
   
-  X.or <- LaplacesDemon::rmatrixnorm(M=M, U=Sigma, V=Psi)
+  X.or <- matrixNormal::rmatrixnorm(M=M, U=Sigma, V=Psi)
   return(list(Y=X.or))
 }
 
@@ -123,7 +131,7 @@ GeraMatrixFull<-function(M=M, coords, phi, rho, sigma2, Sigma, corr="exponential
 #### Beta full matrix and M=Beta*X
 ################################################################################
 
-ML.MatrixRegreSPatioT<-function(dados, X, coords, precision=0.0000001, MaxIter=50, corr="exponential"){
+ML.MatrixRegreSPatioT<-function(dados, X, coords, precision=0.0000001, MaxIter=100, corr="exponential",nu = NULL){
   
   ## Coords: Matrix of coordinates of dim L*2
   ## Dados: A matrix of dimension p*r
@@ -142,24 +150,32 @@ ML.MatrixRegreSPatioT<-function(dados, X, coords, precision=0.0000001, MaxIter=5
   # initial values
   
   Beta<-dados%*%t(X)%*%solve(X%*%t(X))
-  
+  #  Beta[2, 3] <- 0
+  #  Beta[2, 2] <- 0 
   rho<- 0.5
-  phi<- 1
+  phi<- 0.1 #*min(dist(coords))  # Better init
   sigma2<- 1
+  #corr="gaussian"
+  
   #construct matrix 
   PsiT<- ar1_cor (TT, rho = rho)
-  PsiScorr<-spatial_corr_matrix(coords, corr_fun = corr, phi)
+  PsiScorr<-spatial_corr_matrix(coords, corr_fun = corr, phi,nu)
   PsiS<- PsiScorr*sigma2
   Sigma<- diag(p)
+  Sigma[1, 1] <- 1  # Fix reference variance
   Psi<- kronecker(PsiS, PsiT)
   Psicorr<-kronecker(PsiScorr, PsiT)
   #Psicorr <- (Psicorr + t(Psicorr)) / 2  # symmetry
+  
+  
   
   while(criterio > precision){
     
     count <- count + 1
     ##update beta
     Beta<-dados%*%solve(Psi)%*%t(X)%*%solve(X%*%solve(Psi)%*%t(X))
+    #Beta[2, 3] <- 0
+    # Beta[2, 2] <- 0 
     mu<-Beta%*%X
     
     #update sigma2 
@@ -169,21 +185,24 @@ ML.MatrixRegreSPatioT<-function(dados, X, coords, precision=0.0000001, MaxIter=5
     #update Sigma
     Sigma<-1/r*(dados-mu)%*%solve(Psi)%*%t(dados-mu)
     Sigma[1,1]<-1
-    Sigma <- (Sigma + t(Sigma)) / 2
+    #Sigma <- (Sigma + t(Sigma)) / 2 # symmetry
     #update rho
     PsiS<- PsiScorr*sigma2
     #PsiS <- (PsiS + t(PsiS)) / 2  # symmetry
-    Erho<-optimize(loglikelSpatiorho, c(0.0001,0.999), tol = 0.0000000001, maximum = TRUE, PsiS, Sigma, dados, mu, TT)
+    Erho<-optimize(loglikelSpatiorho, c(0.0001,0.999), tol = 1e-12, maximum = TRUE, PsiS, Sigma, dados, mu, TT)
     rho<-Erho$maximum
     #update phi
     PsiT<- ar1_cor (TT, rho = rho)
     #PsiT <- (PsiT + t(PsiT)) / 2  # symmetry
-    Ephi<-optimize(loglikelSpatiophi, c(0.001,10), tol = 0.0000000001, maximum = TRUE, PsiT, Sigma, sigma2, dados, coords, mu,  corr=corr)
+    Ephi<-optimize(loglikelSpatiophi, c(0,2000), tol = 1e-12, maximum = TRUE, PsiT, Sigma, sigma2, dados, coords, mu,  corr=corr,nu = nu)
     phi<-Ephi$maximum
+    
+    
+    
     
     #loop until log-likelihood falls below precision
     loglik[count]<-Ephi$objective
-    PsiScorr<-spatial_corr_matrix(coords, corr_fun = corr, phi)
+    PsiScorr<-spatial_corr_matrix(coords, corr_fun = corr, phi,nu)
     PsiS<- PsiScorr*sigma2
     Psi<-kronecker(PsiS, PsiT)
     #Psi <- (Psi + t(Psi)) / 2  # symmetry
@@ -196,7 +215,7 @@ ML.MatrixRegreSPatioT<-function(dados, X, coords, precision=0.0000001, MaxIter=5
     }
     
     if (count==MaxIter){
-      criterio <- 0.000000000000001
+      criterio <- 1e-12
     }
     
     
@@ -204,9 +223,10 @@ ML.MatrixRegreSPatioT<-function(dados, X, coords, precision=0.0000001, MaxIter=5
   # Beta, rho, phi, sigma2, Sigma
   #compute criteria
   npar <- (p*s)+1+1+1+p*(p+1)/2
+  #npar <- (p*s)+1+1+1+p*(p+1)/2
   BIC   <- -2*loglik[count] + npar*log(p*TT*L)    # to be minimized
   
-  obj.out <- list(Beta=Beta, rho=rho, phi=phi, sigma2=sigma2, Sigma=Sigma, loglik=loglik, BIC=BIC, iter = count)
+  obj.out <- list(Beta=Beta, rho=rho, phi=phi, sigma2=sigma2, Sigma=Sigma,nu=nu, loglik=loglik, BIC=BIC, iter = count)
   
   class(obj.out) <- "ML.MatrixRegreSPatioT"
   return(obj.out)
@@ -214,6 +234,4 @@ ML.MatrixRegreSPatioT<-function(dados, X, coords, precision=0.0000001, MaxIter=5
 }
 
 
-#posterior_df <- as.data.frame(stan_exp_exp_model)
-#stan_exp_exp_model["beta0"]
-#mean(posterior_df$nugget)
+
